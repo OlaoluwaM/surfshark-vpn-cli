cd204c930a444b12bdafc8b5b1e7795270ea4bd0
OlaoluwaM <37044906+OlaoluwaM@users.noreply.github.com>
2021-08-01 11:31:48 +0100

    feat: Improved script
    * Made arguments user inputs
    
    * Added timeouts and default values
    
    * Added checks for necessary data and early exit on absence
    of that data

--- connectToSurfsharkVPN.sh
+++ connectToSurfsharkVPN.sh
@@ -1,15 +1,28 @@
 #!/usr/bin/env bash
 
-locationArgument=$1
-tcpOrUdpArgument=$2
+read -e -t 10 -p 'Where would you like to connect to: ' -n 8 -i 'us-atl' locationArgument
+echo
+read -e -t 10 -p 'Would you like to use tcp or udp: ' -i 'tcp' tcpOrUdpArgument
+echo
 
 location="${locationArgument:=us-atl}"
 tcpOrUdp="${tcpOrUdpArgument:=tcp}"
+envFilePath="$(dirname $0)/.env"
 
 # To grab .env variables
 set -o allexport
-. "$(dirname $0)/.env"
+. $envFilePath
 set +o allexport
+
+if [[ -z "$SURFSHARK_USERNAME" ]]; then
+  read -t 20 -p 'Surfshark username not set in .env, please provide it: ' SURFSHARK_USERNAME
+  [[ -z "$SURFSHARK_USERNAME" ]] && echo "Script cannot run without surfshark username" && exit 1
+
+elif [[ -z "$SURFSHARK_PASSWORD" ]]; then
+  read -t 20 -sp 'Surfshark password not set in .env, please provide it: ' SURFSHARK_PASSWORD
+  [[ -z "$SURFSHARK_PASSWORD" ]] && echo "Script cannot run without surfshark password" && exit 1
+
+fi
 
 expect <(
   cat <<EOF
